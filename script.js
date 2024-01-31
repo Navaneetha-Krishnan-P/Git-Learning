9023471b2affe6960152564024fc5fd1232badfd
Navaneetha-Krishnan-P <krissnank89038@gmail.com>
2024-01-31 12:36:51 +0530

    day 3 task

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+var req;
+request.onload=function(){
+    var req = JSON.parse(request.response);
+    console.log(req);
+
+    for(var i=0;i<=req.length;i++){
+        console.log(req[i].flags)
+     }
+}
+
